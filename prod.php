22d2b8cdd4dc533b1ac356b740812bbc22c245c7
ana <jacintojogo@gmail.com>
2026-05-26 08:47:40 -0300

    Add files via upload

--- prod.php
+++ prod.php
@@ -1,40 +1,50 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
-    <link rel="stylesheet" href="style.css">
-</head>
+
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>Minha Loja</title>
+<title>Unistore</title>
 
-    <style>
-    </style>
+<!-- FONTES -->
+<link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@500;700&family=Poppins:wght@300;400;500&display=swap" rel="stylesheet">
+
+<!-- CSS EXTERNO -->
+<link rel="stylesheet" href="style.css">
+
 </head>
-<body>
-    <nav class="navbar">
-    <div class="logo">
-        <img src="https://media.tenor.com/OoG1CF2T3QIAAAAi/kucing-scuba-scuba-cat.gif" alt="Logo">
-    </div>
-    <div class="nav-links">
-        <a href="#">Produtos</a>
-        <a href="#">Dúvidas</a>
-        <a href="#">Contato</a>
-    </div>
 
-    <div class="nav-actions">
-        <button class="btn-signin" onclick="window.location.href='login.php'">Sign in</button>
-        <button class="btn-register" onclick="window.location.href='cadastro.php'">Register</button>
+<body>
+
+<header>
+
+    <div class="logo">
+        <img src="https://media.tenor.com/OoG1CF2T3QIAAAAi/kucing-scuba-scuba-cat.gif" alt="Logo" style="width: 60px; height: 60px; object-fit: contain;">
     </div>
+    <nav>
+        <a href="#">Produtos</a>
+        <a href="#">Categorias</a>
     </nav>
 
-    <div class="sort-bar">
-        <button class="sort-btn ativo">Novo</button>
-        <button class="sort-btn">Maior preço</button>
-        <button class="sort-btn">Menor preço</button>
+    <div class="buttons">
+
+        <a href="login_cliente.php" class="btn btn-login">Cliente</a>
+        <a href="login_vendedor.php" class="btn btn-login">Vendedor</a>
+        <a href="cadastro.php" class="btn btn-register">Criar Conta</a>
+
     </div>
 
-    <div class="produto-grid">
+</header>
+
+<div class="sort-bar">
+    <button class="sort-btn ativo">Novo</button>
+    <button class="sort-btn">Maior preço</button>
+    <button class="sort-btn">Menor preço</button>
+</div>
+
+<div class="produto-grid">
 
     <div class="produto-card">
         <div class="produto-imagem"><img src="https://i.pinimg.com/736x/cb/33/9a/cb339a683738c3e97a95f37d377d69f3.jpg" alt="MLP"></div>
@@ -67,6 +77,8 @@
             <p class="produto-preco">$0</p>
         </div>
     </div>
+
 </div>
+
 </body>
 </html>
